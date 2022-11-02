5ef98f252a7a856d3fba8b38ac89694185b31419
Mustapha Hamze <58133128+mustapha-hamze@users.noreply.github.com>
2022-11-02 18:51:26 +0300

    init

---
+++ TestDevOpsAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TestDevOpsAzure.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
